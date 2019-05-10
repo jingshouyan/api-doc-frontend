3c185d96f0ac07dd43959f6963dbadcca06a6c96
靖守彦 <290173092@qq.com>
2019-05-10 18:43:55 +0800

    feat(model): InterfaceInfo ...

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -1,4 +1,6 @@
-export class Code {
+import { map } from 'underscore';
+
+export class CodeInfo {
     code: number;
     message: string;
     whoUse: string;
@@ -10,7 +12,7 @@ export class Code {
     }
 }
 
-export class Server {
+export class ServerInfo {
     name: string;
     version: string;
     host: string;
@@ -18,11 +20,82 @@ export class Server {
     instance: string;
 
     constructor(opt: any = {}) {
-        this.name = opt && opt.name && '';
-        this.version = opt && opt.version && '';
-        this.host = opt && opt.host && '';
-        this.port = opt && +opt.port && 0;
-        this.instance = opt && opt.instance && '';
+        this.name = opt && opt.name || '';
+        this.version = opt && opt.version || '';
+        this.host = opt && opt.host || '';
+        this.port = opt && +opt.port || 0;
+        this.instance = opt && opt.instance || '';
     }
+}
 
+export class FieldInfo {
+    name: string;
+    type: string;
+    remark: string;
+    annotations: string[];
+
+    constructor(opt: any = {}) {
+        this.name = opt && opt.name || '';
+        this.type = opt && opt.type || '';
+        this.remark = opt && opt.remark || '';
+        this.annotations = opt && opt.annotations || [];
+    }
+}
+
+export class TypeInfo {
+    type: string;
+    remark: string;
+    fields: FieldInfo[];
+    annotations: string[];
+
+    constructor(opt: any = {}) {
+        this.type = opt && opt.type || '';
+        this.remark = opt && opt.remark || '';
+        this.annotations = opt && opt.annotations || [];
+        this.fields = opt
+            && map(opt.fields, (f: any) => new FieldInfo(f))
+            || [];
+    }
+}
+
+export class BeanInfo {
+    rootType: string;
+    types: TypeInfo[];
+
+    constructor(opt: any = {}) {
+        this.rootType = opt && opt.rootType || '';
+        this.types = opt
+            && map(opt.types, (t: any) => new TypeInfo(t))
+            || [];
+    }
+}
+
+export class MethodInfo {
+    name: string;
+    remark: string;
+    input: BeanInfo;
+    output: BeanInfo;
+
+    constructor(opt: any) {
+        this.name = opt && opt.name || '';
+        this.remark = opt && opt.remark || '';
+        this.input = opt && opt.input && new BeanInfo(opt.input);
+        this.output = opt && opt.output && new BeanInfo(opt.output);
+    }
+}
+
+export class InterfaceInfo {
+    serverInfo: ServerInfo;
+    methodInfos: MethodInfo[];
+    codeInfos: CodeInfo[];
+
+    constructor(opt: any = {}) {
+        this.serverInfo = opt && opt.serverInfo && new ServerInfo(opt.serverInfo);
+        this.methodInfos = opt
+            && map(opt.methodInfos, (m: any) => new MethodInfo(m))
+            && [];
+        this.codeInfos = opt
+            && map(opt.codeInfos, (c: any) => new CodeInfo(c))
+            && [];
+    }
 }
