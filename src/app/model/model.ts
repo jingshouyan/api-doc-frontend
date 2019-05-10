c7a83be89bf25821f8463c05c620e62f13d60aad
靖守彦 <290173092@qq.com>
2019-05-10 18:07:00 +0800

    feat(model): add server

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -4,8 +4,25 @@ export class Code {
     whoUse: string;
 
     constructor(opt: any = {}) {
-        this.code = opt && opt.code || 0;
+        this.code = opt && +opt.code || 0;
         this.message = opt && opt.message || '';
         this.whoUse = opt && opt.whoUse || '';
     }
+}
+
+export class Server {
+    name: string;
+    version: string;
+    host: string;
+    port: number;
+    instance: string;
+
+    constructor(opt: any = {}) {
+        this.name = opt && opt.name && '';
+        this.version = opt && opt.version && '';
+        this.host = opt && opt.host && '';
+        this.port = opt && +opt.port && 0;
+        this.instance = opt && opt.instance && '';
+    }
+
 }
