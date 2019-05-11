c378af2c7d0e704a5d97818eeeaff81a8e60e19f
Jingshouyan <290173092@qq.com>
2019-05-11 22:55:24 +0800

    feat(server): finish server

--- src/app/layout/pages/server/server.component.ts
+++ src/app/layout/pages/server/server.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { ServerInfo, MethodInfo } from 'src/app/model/model';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-server',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+  server: ServerInfo;
+  methods: MethodInfo[];
+
+  columnNames = [
+    {id: 'name', value: 'field'},
+    {id: 'type', value: 'type'},
+    {id: 'annotations', value: 'annotations'},
+    // {id: 'remark', value: 'remark'},
+  ];
+
+  displayedColumns: string[] = _.map(this.columnNames, x => x.id);
+
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.route.params
+    .subscribe(param => this.getHero());
+  }
+
+  getHero() {
+    const serverName = this.route.snapshot.paramMap.get('name');
+    this.apiService.getServer(serverName)
+    .subscribe(x => {
+      this.server = x.serverInfo;
+      this.methods = x.methodInfos;
+    });
   }
 
 }
